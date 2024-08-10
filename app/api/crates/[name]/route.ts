0bd87f76051fc4ef8ed0e96e08ace851cbeebd7c
xizheyin <xizheyin@smail.nju.edu.cn>
2024-08-10 20:29:04 +0800

    Add Version Selector, adjust UI.
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- app/api/crates/[name]/route.ts
+++ app/api/crates/[name]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import pool from '../../../lib/db';
 interface CrateInfo {
     name: string;
-    version: string;
+    versions: string[];
     description: string;
     repository: string;
     downloads: number;
@@ -35,66 +35,6 @@ interface Vulnerability {
     severity: string;
 }
 
-const crateInfo: CrateInfo = {
-    name: 'example-crate',
-    version: '1.0.0',
-    description: 'This is an example crate.',
-    repository: 'https://github.com/example/example-crate',
-    downloads: 12345,
-    maintainers: [
-        { name: 'John Doe', email: 'john@example.com' },
-        { name: 'Jane Smith', email: 'jane@example.com' },
-    ],
-    documentation: 'https://docs.example.com/example-crate',
-    publishedDate: '2023-01-01',
-    licenses: ['MIT', 'Apache-2.0'],
-    dependencyLicenses: {
-        'MIT': 5,
-        'Apache-2.0': 3,
-    },
-    links: {
-        homepage: 'https://example.com',
-        repository: 'https://github.com/example/example-crate',
-    },
-    dependencies: [],
-    vulnerabilities: []
-};
-
-const dependencies: Dependency[] = [
-
-    { name: 'dep1', version: '1.0.0' },
-    { name: 'dep2', version: '2.0.0' },
-];
-
-const vulnerabilities: Vulnerability[] = [
-    {
-        id: 'VULN-001',
-        title: 'Example Vulnerability',
-        description: 'This is an example vulnerability.',
-        severity: 'high',
-    },
-];
-
-// export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
-//     const { name } = params;
-
-//     if (!name || typeof name !== 'string') {
-//         return NextResponse.json({ error: 'Invalid crate name' }, { status: 400 });
-//     }
-
-//     // Here you can add logic to fetch the actual data based on the crate name
-//     // For now, we'll return the example data
-
-//     return NextResponse.json({
-//         crateInfo,
-//         dependencies,
-//         vulnerabilities,
-//     });
-// }
-
-// export async function POST(req: NextRequest) {
-//     return NextResponse.json({ error: 'Method POST Not Allowed' }, { status: 405 });
-// }
 
 
 export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
@@ -115,7 +55,7 @@ export async function GET(req: NextRequest, { params }: { params: { name: string
       const crate = res.rows[0];
       const crateInfo: CrateInfo = {
         name: crate.name,
-        version: crate.version,
+        versions: crate.versions,
         description: crate.description,
         repository: crate.repository,
         downloads: crate.downloads,
--- app/programs/[name]/page.tsx
+++ app/programs/[name]/page.tsx
@@ -9,43 +9,40 @@ import MaintainersList from '../../../components/MaintainersList';
 import DependenciesList, { Dependency } from '../../../components/DependenciesList';
 import DependencyGraph from '../../../components/DependencyGraph';
 import VulnerabilitiesList, { Vulnerability } from '../../../components/VulnerabilitiesList';
-import CrateInfoHeader from '../../../components/CrateInfoHeader';
 import SecurityAdvisories from '../../../components/SecurityAdvisories';
 import LicensesInfo from '../../../components/LicensesInfo';
 import MetadataSection from '@/components/MetadataSection';
+
+import VersionsSelector from '../../../components/VersionsSelector';
 
 const CratePage = () => {
     const [crateInfo, setCrateInfo] = useState<CrateInfo | null>(null);
     const [dependencies, setDependencies] = useState<Dependency[]>([]);
     const [vulnerabilities, setVulnerabilities] = useState<Vulnerability[]>([]);
+    const [versions, setVersions] = useState<string[]>([]);
     const { name } = useParams();
     const router = useRouter();
 
     useEffect(() => {
         if (name) {
             fetch(`/api/crates/${name}`, {
-                method: 'GET', // 明确指定使用 GET 方法
+                method: 'GET',
             })
-
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! status: ${response.status}`);
-                    }
-                    return response.text(); // 先返回文本
-                })
-                .then(text => {
-                    try {
-                        const data = JSON.parse(text); // 手动解析 JSON
-                        setCrateInfo(data.crateInfo || null);
-                        setDependencies(data.dependencies || []);
-                        setVulnerabilities(data.vulnerabilities || []);
-                    } catch (error) {
-                        console.error('Error parsing JSON:', error, 'Response text:', text);
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
+            .then(response => {
+                if (!response.ok) {
+                    //throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json(); 
+            })
+            .then(data=>{
+                setCrateInfo(data.crateInfo || {});
+                setDependencies(data.dependencies || []);
+                setVulnerabilities(data.vulnerabilities || []);
+                setVersions(data.versions || []);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
         }
     }, [name]);
 
@@ -58,8 +55,7 @@ const CratePage = () => {
             <Header onBack={() => router.back()} />
             <main className="flex-grow overflow-y-auto bg-gray-100">
                 <div className="container mx-auto p-4 flex">
-                    <div className="w-full md:w-2/3 pr-2">
-                        <CrateInfoHeader name={crateInfo.name} version={crateInfo.version} />
+                    <div className="w-full md:w-2/3 pr-2">                        
                         <CrateInfoCard crateInfo={crateInfo} />
                         <MaintainersList maintainers={crateInfo?.maintainers || []} />
                         <SecurityAdvisories vulnerabilities={vulnerabilities} />
@@ -71,6 +67,7 @@ const CratePage = () => {
                         />
                     </div>
                     <div className="w-full md:w-1/3 pl-2 border-l-2">
+                        <VersionsSelector versions={versions} currentVersion={crateInfo.version} crateName={crateInfo.name} />
                         <VulnerabilitiesList vulnerabilities={vulnerabilities} />
                         <DependenciesList dependencies={dependencies} />
                         <DependencyGraph dependencies={dependencies} />
