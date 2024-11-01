053df33cf6359b4bfec648971630ef2bfb4b29bd
mapan-nju <2246839805@qq.com>
2024-11-01 21:31:41 +0800

    前后端连接

--- app/api/crates/route.ts
+++ app/api/crates/route.ts
@@ -3,13 +3,11 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
   try {
     // 发送 HTTP 请求获取外部数据
-    const externalApiUrl = 'http://210.28.134.203:6888/crates'; // 替换为你的外部 API URL
+    const externalApiUrl = 'http://210.28.134.203:6888/api/crates'; // 替换为你的外部 API URL
     const externalRes = await fetch(externalApiUrl);
-
     if (!externalRes.ok) {
       throw new Error('Failed to fetch external data');
     }
-
     const externalData = await externalRes.json();
 
     return NextResponse.json(externalData);
@@ -18,3 +16,4 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
+
