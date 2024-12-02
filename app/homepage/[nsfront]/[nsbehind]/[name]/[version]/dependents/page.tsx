55ea9eafae89503e52d253950a1145bf7ecf07af
mapan-nju <2246839805@qq.com>
2024-12-02 22:46:20 +0800

    Fixed the fullscreen rendering bug

--- app/homepage/[nsfront]/[nsbehind]/[name]/[version]/dependents/page.tsx
+++ app/homepage/[nsfront]/[nsbehind]/[name]/[version]/dependents/page.tsx
@@ -1,9 +1,11 @@
+//Dependents页面
 "use client";
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+
 import DependentTable from '../../../../../../../components/DependentTable';
 import { dependentsInfo } from '@/app/lib/all_interface';
 import { useParams } from 'next/navigation';
+
 
 
 const CratePage = () => {
@@ -13,8 +15,7 @@ const CratePage = () => {
 
     const params = useParams();
 
-    const crateName = params.name; // 从 URL 中获取 crate_name 参数
-    const version = params.version; // 从 URL 中获取 version 参数
+
 
     useEffect(() => {
         const fetchCrateData = async () => {
@@ -43,95 +44,10 @@ const CratePage = () => {
     if (loading) return <div>Loading...</div>;
     if (error) return <div className="text-red-500">Error: {error}</div>;
 
-    //console.log('dependencyyyyyyyyyyyyyyy', results?.data);
+
 
     return (
         <div>
-            {/* Existing header and search */}
-            <header className="bg-white shadow p-4">
-                <div className="flex justify-between items-center">
-                    <div className="text-xl font-bold flex flex-col items-start space-y-1">
-                        <Link href="/homepage">
-                            <div className="flex items-center space-x-1">
-                                <span>open</span>
-                                <span className="text-green-500">/</span>
-                                <span>source</span>
-                                <span className="text-green-500">/</span>
-                                <span>insights</span>
-                            </div>
-                        </Link>
-                        <div className="flex items-center space-x-2 mt-15">
-                            <span>{crateName}</span>
-                            <div className="relative">
-                                <button className="flex items-center px-4 py-2 border border-gray-300 rounded hover:bg-gray-100">
-                                    {version}
-                                    <svg
-                                        className="ml-2 w-4 h-4"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                </button>
-                                {/* 这里可以添加版本选择的下拉菜单 */}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <input
-                            type="text"
-                            className="p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Search..."
-                        />
-                        <button className="bg-blue-600 text-white px-4 py-2 rounded-r-md hover:bg-blue-700">
-                            Search
-                        </button>
-                    </div>
-                </div>
-
-                {/* 导航栏 */}
-                <nav className="mt-4">
-                    <ul className="flex space-x-4 text-gray-500 relative">
-                        <li className="cursor-pointer relative">
-                            <Link
-                                href={`/homepage/${params.nsfront}/${params.nsbehind}/${params.name}/${params.version}`}
-                            >
-                                <div className="block py-2 relative z-10">Overview</div>
-                            </Link>
-
-                        </li>
-                        <li className="cursor-pointer relative">
-                            <Link
-                                href={
-                                    `/homepage/${params.nsfront}/${params.nsbehind}/${params.name}/${params.version}/dependencies`}
-                            >
-                                <div className="block py-2 relative z-10">Dependencies</div>
-                            </Link>
-                        </li>
-
-                        <Link
-                            href={`/homepage/${params.nsfront}/${params.nsbehind}/${params.name}/${params.version}/dependents`}
-                        >
-                            <li className="cursor-pointer relative">
-                                <div className="block py-2 relative z-10">Dependents</div>
-                                <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500"></div>
-                            </li>
-                        </Link>
-                        {/* <li className="cursor-pointer relative">
-                            <a href="#" className="block py-2 relative z-10">Compare</a>
-                        </li>
-                        <li className="cursor-pointer relative">
-                            <a href="#" className="block py-2 relative z-10">Versions</a>
-                        </li> */}
-                    </ul>
-                </nav>
-
-            </header>
-
 
             <DependentTable data={results?.data} />
 
