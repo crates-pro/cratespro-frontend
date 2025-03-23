e942e1f93e61f56bc1b93307b0cd4548aa5923dc
mapan-nju <2246839805@qq.com>
2025-03-23 23:59:37 +0800

    overview page (#159)
    * overview page
    
    * debug in commit
    
    * sovle conflict in push
    
    * Bump @types/node from 22.13.9 to 22.13.10 (#151)
    
    Bumps [@types/node](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/node) from 22.13.9 to 22.13.10.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/node)
    
    ---
    updated-dependencies:
    - dependency-name: "@types/node"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump eslint from 9.21.0 to 9.22.0 (#150)
    
    Bumps [eslint](https://github.com/eslint/eslint) from 9.21.0 to 9.22.0.
    - [Release notes](https://github.com/eslint/eslint/releases)
    - [Changelog](https://github.com/eslint/eslint/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/eslint/eslint/compare/v9.21.0...v9.22.0)
    
    ---
    updated-dependencies:
    - dependency-name: eslint
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump autoprefixer from 10.4.20 to 10.4.21 (#148)
    
    Bumps [autoprefixer](https://github.com/postcss/autoprefixer) from 10.4.20 to 10.4.21.
    - [Release notes](https://github.com/postcss/autoprefixer/releases)
    - [Changelog](https://github.com/postcss/autoprefixer/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/postcss/autoprefixer/compare/10.4.20...10.4.21)
    
    ---
    updated-dependencies:
    - dependency-name: autoprefixer
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- components/HeaderWithSearch.tsx
+++ components/HeaderWithSearch.tsx
@@ -1,75 +1,26 @@
 // components/Header.js
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { useHeaderContext } from '../app/context/CrateContext';
-import { useParams } from 'next/navigation';
 import { message } from 'antd';
-import { useRouter } from 'next/navigation';
-// import { cratesInfo } from '@/app/lib/all_interface';
 
 const Header = () => {
-    const router = useRouter();
-    console.log('paramsssssssssssss:', router.refresh);
-    const params = useParams();
-    const [messageApi, contextHolder] = message.useMessage();//antd-message的hooks调用
+    const [messageApi, contextHolder] = message.useMessage();
     const [searchQuery, setSearchQuery] = useState('');
-    const { crateData, setCrateData } = useHeaderContext();
-    const [isOpen, setIsOpen] = useState(false);
-
-
-    const [currentVersion, setCurrentVersion] = useState(params.version); // 存储当前选中的版本
 
     const handleKeyPress = (e: { key: string; }) => {
-        // 检查是否按下了回车键
         if (e.key === 'Enter') {
-            // 如果是回车键，执行搜索
             performSearch();
         }
     };
 
     const performSearch = () => {
         if (!searchQuery || searchQuery.trim() === '') {
-
             messageApi.warning('请输入搜索内容');
-            //alert("请输入搜索内容"); // 可选：提示用户输入内容
         }
         if (searchQuery.trim()) {
-            // 使用 Link 跳转到搜索页面
             window.location.href = `/search?crate_name=${searchQuery}`;
         }
-    };
-    useEffect(() => {
-        const path = params.path || ''; // 确保有值
-        console.log('path:', path);
-    }, [params.path]); // 依赖于 params.path
-    // 使用 useEffect 从 API 获取数据
-    useEffect(() => {
-        const fetchData = async () => {
-            // 如果 crateData.results 为空，说明数据还未加载
-            if (!crateData.results) {
-                const response = await fetch(`/api/crates/${params.nsfront}/${params.nsbehind}/${params.name}/${params.version}`);
-                const data = await response.json();
-                console.log('dataaaaaaaaaaaaaa:', data);
-                setCrateData({
-                    crateName: data.crate_name,
-                    crateVersion: params.version,
-                    results: data,
-                });
-
-            }
-        };
-
-        fetchData();
-    }, [params.nsfront, params.nsbehind, params.name, params.version, setCrateData, crateData.crateVersion, crateData.results]); // 添加 crateData 作为依赖项
-
-    const toggleDropdown = () => {
-        setIsOpen((prev) => !prev);
-    };
-
-    const closeDropdown = () => {
-        setIsOpen(false);
-
     };
 
     return (
@@ -77,7 +28,7 @@ const Header = () => {
             {contextHolder}
             <header className="bg-white shadow p-4">
                 <div className="flex justify-between items-center">
-                    <div className="text-xl font-bold flex flex-col items-start space-y-1">
+                    <div className="text-xl font-bold">
                         <Link href="/">
                             <div className="text-xl font-bold flex items-center space-x-1">
                                 <span style={{ color: 'rgb(57,62,70)' }}>open</span>
@@ -87,55 +38,8 @@ const Header = () => {
                                 <span style={{ color: 'rgb(57,62,70)' }}>insights</span>
                             </div>
                         </Link>
-                        <div className="flex items-center space-x-2 mt-15">
-                            <span className="text-2xl" style={{ color: 'rgb(28, 63, 115)' }}>{params.name}</span>
-                            <div className="relative">
-                                <button
-                                    onClick={toggleDropdown}
-                                    className="flex items-center px-4 py-2 border border-gray-300 rounded hover:bg-gray-100"
-                                >
-                                    {/* {crateData.crateVersion || 'Select Version'} */}
-                                    {currentVersion || 'Select Version'}
-                                    <svg
-                                        className="ml-2 w-4 h-4"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                </button>
-                                {isOpen && (
-                                    <div className="absolute mt-1 w-full">
-                                        <div className="absolute inset-0 bg-black opacity-50" onClick={closeDropdown}></div>
-                                        <div className="relative bg-white border border-gray-300 rounded shadow-lg z-20">
-                                            <ul className="max-h-60 overflow-y-auto">
-                                                {crateData.results?.versions.map((version, index) => (
-                                                    <Link
-                                                        key={index}
-                                                        onClick={() => {
-                                                            setCurrentVersion(version);
-                                                        }}
-                                                        href={`/${params.nsfront}/${params.nsbehind}/${crateData.results?.crate_name}/${version}`}
-                                                    >
-                                                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-
-                                                            {version}
-
-                                                        </li>
-                                                    </Link>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
                     </div>
-                    <div className="flex items-center mb-4">
+                    <div className="flex items-center">
                         <input
                             type="text"
                             placeholder="Search for open source crates"
@@ -152,7 +56,6 @@ const Header = () => {
                 </div>
             </header>
         </>
-
     );
 };
 
